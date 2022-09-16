5545627755a4df09844ae22532cb314b4f7a2edd
tuliany <tuliany@mail.com>
2022-09-16 14:46:02 +0200

    added background-color to nav

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import Navbar from './components/Navbar';
 import Divider from './components/Divider';
 import About from './pages/About';
 import Team from './pages/Team';
+import BurgerMenu from './components/BurgerMenu';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Navbar />
+        <Divider />
       </header>
       <div className="frame">
-        <Divider />
         <About />
         <Divider />
         <Team />
