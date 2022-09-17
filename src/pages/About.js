de88063e77e5cf252e6c27633d12af251d965f57
tuliany <tuliany@mail.com>
2022-09-17 04:01:16 +0200

    pad team

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,15 +7,17 @@ import Section from "../components/Section";
 const About = () => {
   return (
     <Section>
-      <div className="f-container">
-        <h1 className="title">About</h1>
-        <h3 className="breadText">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </h3>
-      </div>
-      <div className="container">
-        <img src="../../assets/paint.png" alt="painting" className="image" />
+      <div className="aboutFrame">
+        <div className="f-container">
+          <h1 className="title">About</h1>
+          <h3 className="breadText">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </h3>
+        </div>
+        <div className="container">
+          <img src="../../assets/paint.png" alt="painting" className="image" />
+        </div>
       </div>
     </Section>
   );
