9fde8417760f378725dd43a8466fd27ed5f07439
tuliany <tuliany@mail.com>
2022-09-19 09:26:06 +0200

    mobile footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,14 +7,16 @@ const Footer = () => {
   return (
     <>
       <Section>
-        <div className="logoFrame">
-          <img src="../../assets/facebook.png" alt="" className="icon" />
-          <img src="../../assets/twitter.png" alt="" className="icon" />
-          <img src="../../assets/linkedin.png" alt="" className="icon" />
-          <img src="../../assets/youtube.png" alt="" className="icon" />
+        <div className="footerFrame">
+          <div className="logoFrame">
+            <img src="../../assets/facebook.png" alt="" className="icon" />
+            <img src="../../assets/twitter.png" alt="" className="icon" />
+            <img src="../../assets/linkedin.png" alt="" className="icon" />
+            <img src="../../assets/youtube.png" alt="" className="icon" />
           </div>
           <img src="../../assets/hexagon.png" alt="" className="footerLogo" />
           <h1>Company</h1>
+        </div>
       </Section>
     </>
   );
